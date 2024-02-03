e3ee2bc063d55f4b972d689b53fcc13dcb2bd4cd
coderheck <loxzyioxzybusiness@hotmail.com>
2024-02-03 21:42:17 +0700

    Built and published by Moonwave

---
+++ assets/js/d24dad7f.1d9e67c6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[433],{21013:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":":Write","id":"Write","level":3}]')}}]);
