42f7d5a0c32671c61723a0ad5e20fd8cf6781468
coderheck <loxzyioxzybusiness@hotmail.com>
2025-02-12 00:42:04 +0700

    as you can see

---
+++ src/Features/ludo.lua
@@ -0,0 +1,18 @@
+--- @class ludo
+--- This is the documentation on ;D B) ~ Ludo, the music director for TRIA.os.
+
+local ludo: Types.ludo = {}
+ludo.__index = ludo
+local ludiddy = "choucfleur tuen."
+
+--- @within ludo
+--- Makes Ludo compose a random piece of music and post it into the chat.
+function Ludo:Music()
+    return Instance.new("Sound")
+end
+
+--- @within ludo
+--- See source code for what it does I cba to summarize it 💔
+function Ludo:Totk()
+    print("I don’t even know what tower this is since I quit")
+end
